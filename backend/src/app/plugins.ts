0dc14428d62eb402ebc2ba426a8017a0668b51b3
lemii <lemiidelegate@gmail.com>
2021-07-12 16:24:11 +0200

    add http plugin

--- backend/src/app/plugins.ts
+++ backend/src/app/plugins.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Application } from 'lisk-sdk';
-import { LcApiPlugin } from "./plugins/lc_api/lc_api_plugin";
+import { Application, HTTPAPIPlugin } from 'lisk-sdk';
+import { LcApiPlugin } from './plugins/lc_api/lc_api_plugin';
 
 // @ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerPlugins = (app: Application): void => {
-
-    app.registerPlugin(LcApiPlugin);
+	app.registerPlugin(HTTPAPIPlugin);
+	app.registerPlugin(LcApiPlugin);
 };
